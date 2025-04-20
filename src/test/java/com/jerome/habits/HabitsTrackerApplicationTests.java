02dbdc86619e73298722b97efc7e58aef66d3df4
Jerds <jeromeyang21@gmail.com>
2025-04-20 16:05:27 +0800

    init

---
+++ src/test/java/com/jerome/habits/HabitsTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jerome.habits;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabitsTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
